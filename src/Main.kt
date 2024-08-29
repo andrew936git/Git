491840cd5d43281cff51ec7bd871136ec4ad8896
andrew936git <936and@gmail.com>
2024-08-29 11:33:51 +0500

    Создан enum класс Phone

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+}
+
+enum class Phone(val value: String){
+    model1("Apple iPhone SE 2024 64Gb"),
+    model2("Apple iPhone 15 Pro 512Gb"),
+    model3("Xiaomi Redmi A3 64Gb"),
+    model4("HONOR X8a 128Gb"),
+    model5("Samsung Galaxy S24 Ultra 1024Gb")
+}
