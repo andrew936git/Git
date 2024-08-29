0af862bfad1d5bb39b0cf508946409184ad74887
andrew936git <936and@gmail.com>
2024-08-29 11:33:51 +0500

    Измнил enum класс Phone на PhonePriceInCityN создал enum класс PhonePriceInCityM

--- src/Main.kt
+++ src/Main.kt
@@ -4,10 +4,18 @@ fun main() {
 
 }
 
-enum class Phone(val value: String){
-    model1("Apple iPhone SE 2024 64Gb"),
-    model2("Apple iPhone 15 Pro 512Gb"),
-    model3("Xiaomi Redmi A3 64Gb"),
-    model4("HONOR X8a 128Gb"),
-    model5("Samsung Galaxy S24 Ultra 1024Gb")
+enum class PhonePriceInCityN(val value: String, price: Int){
+    model1("Apple iPhone SE 2024 64Gb", 31450),
+    model2("Apple iPhone 15 Pro 512Gb", 125515),
+    model3("Xiaomi Redmi A3 64Gb", 6999),
+    model4("HONOR X8a 128Gb", 19069),
+    model5("Samsung Galaxy S24 Ultra 1024Gb", 115000)
+}
+
+enum class PhonePriceInCityK(val value: String, price: Int){
+    model1("Apple iPhone SE 2024 64Gb", 30450),
+    model2("Apple iPhone 15 Pro 512Gb", 126515),
+    model3("Xiaomi Redmi A3 64Gb", 7199),
+    model4("HONOR X8a 128Gb", 19869),
+    model5("Samsung Galaxy S24 Ultra 1024Gb", 118000)
 }
